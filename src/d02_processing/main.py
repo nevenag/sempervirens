34dd186e6d4e590dbfa5b98d07b5f90366f966d5
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2022-09-17 14:03:46 -0700

    test

--- src/d02_processing/main.py
+++ src/d02_processing/main.py
@@ -45,18 +45,47 @@ def forward_pass(pivot, prob_mat):
     num_rows = prob_mat.shape[0]
     column_map = np.array(range(prob_mat.shape[1])) # column_map[i] gives index of column in original prob_mat
 
+    # union_count = 0
     while prob_mat.shape[1] > 0: # While columns remaining.
+        p_eq = prob_equal(pivot, prob_mat)
+        p_sup = prob_superset(pivot, prob_mat) - p_eq
+        p_sub = prob_subset(pivot, prob_mat) - p_eq
         p_empty_intersect = prob_empty_intersect(pivot, prob_mat)
-        p_other = 1 - p_empty_intersect
-        joined = np.vstack((p_empty_intersect, p_other))
-        entropy = scipy.stats.entropy(joined, base=2, axis=0)
+        denom = p_eq + p_sup + p_sub + p_empty_intersect
+        p_eq /= denom
+        p_sup /= denom
+        p_sub /= denom
+        p_empty_intersect /= denom
+        joined = np.vstack((p_sup, p_eq, p_sub, p_empty_intersect))
+        assert np.all(joined <= 1) and np.all(0 <= joined)
+        assert np.all(np.isclose(np.sum(joined, axis=0), 1))
+        entropy = scipy.stats.entropy(joined, base=4, axis=0)
         min_idx = np.argmin(entropy)
-        if joined[0, min_idx] > joined[1, min_idx]: # Probability of intersection being empty is greater.
+        max_prob_idx = np.argmax(joined[:, min_idx])
+        min_entropy_col = prob_mat[:, min_idx].reshape((num_rows, 1))
+        if max_prob_idx == 0: # sup
+            pass
+        elif max_prob_idx == 1: # eq # ISSUE: this never seems to be largest...
+            pivot = 1 - (1-pivot)*(1-min_entropy_col)
+        elif max_prob_idx == 2: # sub
+            pivot = min_entropy_col
+        else: # empty intersection
             disjoints[0, column_map[min_idx]] = 1
-        else:
-            min_entropy_col = prob_mat[:, min_idx].reshape((num_rows, 1))
-            pivot = 1 - (1-pivot)*(1-min_entropy_col) # Probabilistic union
-            assert pivot.shape == (prob_mat.shape[0], 1)
+        assert pivot.shape == (prob_mat.shape[0], 1)
+
+        # p_empty_intersect = prob_empty_intersect(pivot, prob_mat)
+        # p_other = 1 - p_empty_intersect
+        # joined = np.vstack((p_empty_intersect, p_other))
+        # entropy = scipy.stats.entropy(joined, base=2, axis=0)
+        # min_idx = np.argmin(entropy)
+        # if joined[0, min_idx] > joined[1, min_idx]: # Probability of intersection being empty is greater.
+        #     disjoints[0, column_map[min_idx]] = 1
+        # else:
+        #     # union_count += 1
+        #     min_entropy_col = prob_mat[:, min_idx].reshape((num_rows, 1))
+        #     pivot = 1 - (1-pivot)*(1-min_entropy_col) # Probabilistic union
+        #     # pivot = pivot*min_entropy_col
+        #     assert pivot.shape == (prob_mat.shape[0], 1)
         prob_mat = np.delete(prob_mat, min_idx, 1)
         column_map = np.delete(column_map, min_idx)
     subsets = 1 - disjoints
@@ -75,15 +104,15 @@ def determine_col(disjoints, subsets, prob_mat, pivot_union, fpr, fnr, true_prio
     # print('prob change disjoints', prob_change_disjoints)
     # print('prob change subsets', prob_change_subsets)
 
-    p10 = fnr*true_prior[1] / (fnr*true_prior[1] + (1-fpr)*true_prior[0])
-    p11 = 1 - (fpr*true_prior[0])/(fpr*true_prior[0]+(1-fnr)*true_prior[1])
-    all0s_p = 1 - np.power(1 - p10, np.sum(subsets) + 1)
-    all1s_p = 1 - np.power(1 - p11, np.sum(subsets) + 1)
+    # p10 = fnr*true_prior[1] / (fnr*true_prior[1] + (1-fpr)*true_prior[0])
+    # p11 = 1 - (fpr*true_prior[0])/(fpr*true_prior[0]+(1-fnr)*true_prior[1])
+    # all0s_p = 1 - np.power(1 - p10, np.sum(subsets) + 1)
+    # all1s_p = 1 - np.power(1 - p11, np.sum(subsets) + 1)
     # print(f'p10: {p10}, p11: {p11}, {(all0s_p + all1s_p)/2}, {all0s_p}, {1/2}')
     # thresh = all1s_p
-    thresh = (all0s_p + all1s_p)/2
+    # thresh = (all0s_p + all1s_p)/2
     # thresh = all0s_p
-    # thresh = 1/2
+    thresh = 1/2
 
     pr1_d = np.prod(1 - prob_mat * disjoints, axis=1)
     pr1_s = pivot_union.reshape((len(pivot_union),))
@@ -127,10 +156,12 @@ def reconstruct(measured, fpr, fnr, true_prior):
         pivot, pivot_idx = pick_pivot(prob_mat)
         prob_mat = np.delete(prob_mat, pivot_idx, 1)
 
+        # print(f'pivot: \n{pivot}')
         disjoints, subsets, pivot_union = forward_pass(pivot, prob_mat)
 
+        # print(f'pivot after unions: \n{pivot_union}')
         true_col = determine_col(disjoints, subsets, prob_mat, pivot_union, fpr, fnr, true_prior)
-
+        # print(f'01d pivot: \n{true_col}')
         prob_mat = backward_pass(prob_mat, true_col, disjoints, subsets)
 
         completed_cols[:, completed_cols_i] = true_col.reshape((true_col.shape[0],))
@@ -148,85 +179,86 @@ def reconstruct(measured, fpr, fnr, true_prior):
     return reconstructed
 
 
-# ## Test with actual data
-# # file_name  = "simNo_1-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000"
+# Test with actual data
+file_name  = "simNo_1-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000"
 # file_name = "simNo_10-s_100-m_1000-h_1-minVAF_0.005-ISAV_0-n_1000-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000"
-# true_data_filename = "data/" + file_name + ".SC.before_FP_FN_NA"
-# measured_data_filename = "data/" + file_name + ".SC"
-# true_data = read_data(true_data_filename)
-# measured_data = read_data(measured_data_filename)
-# # Set missing entries in measured data to true values (only considering false positives and negatives right now).
-# missing_entries = measured_data == 3
-# measured_data[missing_entries] = true_data[missing_entries]
-
-# fpr = 0.001 # False positive rate.
-# fnr = 0.2 # False negative rate.
-# # Priors
-# true_prior = np.array([1/2, 0])
-# true_prior[1] = 1 - true_prior[0]
-
-# reconstructed = reconstruct(measured_data, fpr, fnr, true_prior)
-
-# print(f"Number of elements in matrix: {true_data.shape[0] * true_data.shape[1]}")
-# metrics.isPtree(reconstructed)
-# metrics.compareAD(true_data, reconstructed)
-# metrics.compareDF(true_data, reconstructed)
-# print(f"Number of differences: {metrics.num_diffs(true_data, reconstructed)}")
-
-# for basic testing
-
-rng = np.random.default_rng()
-
-def corrupt(M, fpr, fnr):
-    corrupted = np.copy(M)
-    randoms = rng.uniform(size=M.shape)
-    for i in range(M.shape[0]):
-        for j in range(M.shape[1]):
-            if M[i,j] == 0:
-                if randoms[i,j] <= fpr:
-                    corrupted[i,j] = 1
-            else:
-                if randoms[i,j] <= fnr:
-                    corrupted[i,j] = 0
-    return corrupted
-
-N = 3
-M = 2*N
+true_data_filename = "data/" + file_name + ".SC.before_FP_FN_NA"
+measured_data_filename = "data/" + file_name + ".SC"
+true_data = read_data(true_data_filename)
+measured_data = read_data(measured_data_filename)
+# Set missing entries in measured data to true values (only considering false positives and negatives right now).
+missing_entries = measured_data == 3
+measured_data[missing_entries] = true_data[missing_entries]
 
 fpr = 0.001 # False positive rate.
-fnr = 0.1  # False negative rate.
-
+fnr = 0.2 # False negative rate.
 # Priors
 true_prior = np.array([1/2, 0])
 true_prior[1] = 1 - true_prior[0]
 
-# true_data = np.array([[1, 1, 1, 1], [0, 1, 1, 1], [0, 0, 1, 1], [0, 0, 0, 1], [0, 0, 0, 0]])
-# true_data = np.zeros((N, M))
-# true_data[0, M-1] = 1
-true_data = np.triu(np.ones((N, N)))
-true_data = np.array(np.bmat([[true_data, np.zeros((N, N))], [np.zeros((N, N)), true_data]]))
-# true_data = np.hstack((true_data, true_data))
-# true_data = np.hstack((true_data, true_data))
-
-measured = np.array([
-    [1, 1, 1, 0, 0, 0], 
-    [0, 1, 1, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0 ,0],
-    [0, 0, 0, 0, 1, 1],
-    [0, 0, 0, 0, 0, 1.0]
-])
-# measured = corrupt(true_data, fpr, fnr)
-
-print(f"Measured: \n{measured}")
-
-reconstructed = reconstruct(measured, fpr, fnr, true_prior)
-print(f"Reconstructed: \n{reconstructed}")
-
-# print(metrics.isPtree(reconstructed_cols))
+reconstructed = reconstruct(measured_data, fpr, fnr, true_prior)
 
 print(f"Number of elements in matrix: {true_data.shape[0] * true_data.shape[1]}")
 metrics.isPtree(reconstructed)
 metrics.compareAD(true_data, reconstructed)
 metrics.compareDF(true_data, reconstructed)
 print(f"Number of differences: {metrics.num_diffs(true_data, reconstructed)}")
+
+# # for basic testing
+
+# rng = np.random.default_rng()
+
+# def corrupt(M, fpr, fnr):
+#     corrupted = np.copy(M)
+#     randoms = rng.uniform(size=M.shape)
+#     for i in range(M.shape[0]):
+#         for j in range(M.shape[1]):
+#             if M[i,j] == 0:
+#                 if randoms[i,j] <= fpr:
+#                     corrupted[i,j] = 1
+#             else:
+#                 if randoms[i,j] <= fnr:
+#                     corrupted[i,j] = 0
+#     return corrupted
+
+# N = 5
+# M = 2*N
+
+# fpr = 0.000 # False positive rate.
+# fnr = 0.2  # False negative rate.
+
+# # Priors
+# true_prior = np.array([1/2, 0])
+# true_prior[1] = 1 - true_prior[0]
+
+# # true_data = np.array([[1, 1, 1, 1], [0, 1, 1, 1], [0, 0, 1, 1], [0, 0, 0, 1], [0, 0, 0, 0]])
+# # true_data = np.zeros((N, M))
+# # true_data[0, M-1] = 1
+# true_data = np.triu(np.ones((N, N)))
+# # true_data = np.array(np.bmat([[true_data, np.zeros((N, N))], [np.zeros((N, N)), true_data]]))
+# # true_data = np.hstack((true_data, true_data))
+# # true_data = np.hstack((true_data, true_data))
+# # measured = true_data
+
+# # measured = np.array([
+# #     [1, 1, 1, 0, 0, 0], 
+# #     [0, 1, 1, 0, 0, 0],
+# #     [0, 0, 1, 0, 0, 0],
+# #     [0, 0, 0, 1, 0 ,0],
+# #     [0, 0, 0, 0, 1, 1],
+# #     [0, 0, 0, 0, 0, 1.0]
+# # ])
+# measured = corrupt(true_data, fpr, fnr)
+
+# print(f"Measured: \n{measured}")
+
+# reconstructed = reconstruct(measured, fpr, fnr, true_prior)
+# print(f"Reconstructed: \n{reconstructed}")
+
+# # print(metrics.isPtree(reconstructed_cols))
+
+# print(f"Number of elements in matrix: {true_data.shape[0] * true_data.shape[1]}")
+# metrics.isPtree(reconstructed)
+# metrics.compareAD(true_data, reconstructed)
+# metrics.compareDF(true_data, reconstructed)
+# print(f"Number of differences: {metrics.num_diffs(true_data, reconstructed)}")
---
+++ src/d02_processing/metrics.py
@@ -0,0 +1,68 @@
+# From HUNTRESS code
+
+import numpy as np
+# import modin.pandas as pd
+import pandas as pd
+
+def isPtree(matrix_in):   # brute force check if matrix_in is a pTree
+    M=matrix_in.astype(bool)
+    for j in range(M.shape[1]):
+        for i in range(j,M.shape[1]):
+            cap=M[:,i]*M[:,j]
+            cap_size=np.sum(cap)
+            Mi_size=np.sum(M[:,i])
+            Mj_size=np.sum(M[:,j])
+            if (cap_size != 0):
+                if (cap_size != Mi_size):
+                    if (cap_size != Mj_size):
+                        print("Seems NOT to be a PTree")
+                        return False
+                
+    print("Seems to be a PTree ...")
+    return True
+
+
+
+def compareAD(M1,M2):      # Computes the AD scores for M2 given the ground truth matrix M1
+    error_pairs=[]
+    n_adpairs=0
+    for i in range(M1.shape[1]):
+#        print(i)
+        for j in range(i,M1.shape[1]):
+            cap1=M1[:,i]*M1[:,j]
+            cap2=M2[:,i]*M2[:,j]
+            if (np.sum(cap1)>0 and np.sum(M1[:,i]) != np.sum(M1[:,j])):
+                n_adpairs=n_adpairs+1
+                if (np.sum(cap2)==0):
+                    error_pairs.append([i,j])
+                else:
+                    if (np.sum(M1[:,j])>np.sum(M1[:,i]) and np.sum(M2[:,j])<=np.sum(M2[:,i])):
+                        error_pairs.append([i,j])
+                    else:
+                        if (np.sum(M1[:,i])>np.sum(M1[:,j]) and np.sum(M2[:,i])<=np.sum(M2[:,j])):
+                            error_pairs.append([i,j])
+                        #print(i,j,sum(M1[:,i]),sum(M1[:,j]),sum(M2[:,i]),sum(M2[:,j]))
+    print('Number of AD pairs = ',n_adpairs,"errors : ",len(error_pairs), "AD score = ", 1 - len(error_pairs)/n_adpairs)
+    return error_pairs                
+
+def compareDF(M_orj,M_rec):  # Computes the Diff Lineage scores for M_rec given the ground truth matrix M_orj
+    error_pairs=[]
+    d_pairs=0
+    for i in range(M_orj.shape[1]):
+        for j in range(i,M_orj.shape[1]):
+            cap1=M_orj[:,i]*M_orj[:,j]
+            cap2=M_rec[:,i]*M_rec[:,j]
+            if np.sum(cap1)==0:
+                d_pairs=d_pairs + 1
+                if np.sum(cap2)>0:
+                    error_pairs.append([i,j])
+    
+    if d_pairs == 0:
+        score = 1
+    else:
+        score = 1 - len(error_pairs)/d_pairs
+    print("Number of Diff pairs = ",d_pairs, "errors :",len(error_pairs), "score :", score)
+    return 
+
+def num_diffs(M1, M2):
+    return np.sum(M1 != M2)
