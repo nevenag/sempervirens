e61ccfd458be7163f8eac976dff0f2d694add356
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2022-12-01 17:04:07 -0800

    handle n/a elems by setting to 0 (works bad)

--- src/d02_processing/comparison.py
+++ src/d02_processing/comparison.py
@@ -5,11 +5,14 @@ Comparing this project's algorithm to HUNTRESS
 import numpy as np
 import pandas as pd
 import multiprocessing as mp
+import datetime
+import time
 
 import main
 import huntress
+import CountBasedReconstructor
 import metrics
-
+import file_prefixes
 
 def read_data(filename):     # reads a matrix from file and returns it in BOOL type
     ds = pd.read_csv(filename, sep='\t', index_col=0)
@@ -17,19 +20,25 @@ def read_data(filename):     # reads a matrix from file and returns it in BOOL t
     return M 
 
 
-def load_file(file_prefix: str):
+def load_file(file_prefix: str, only_fpfn = False):
     true_data_filename = "data/" + file_prefix + ".SC.before_FP_FN_NA"
-    measured_data_filename = "data/only_fpfn/" + file_prefix + ".SC"
+    if only_fpfn:
+        measured_data_filename = "data/only_fpfn/" + file_prefix + ".SC"
+    else:
+        measured_data_filename = "data/" + file_prefix + ".SC"
     true_data = read_data(true_data_filename)
     measured_data = read_data(measured_data_filename)
-    assert np.sum(measured_data == 3) == 0
+    # assert np.sum(measured_data == 3) == 0
     # # Set missing entries in measured data to true values (only considering false positives and negatives right now).
     # missing_entries = (measured_data == 3)
     # measured_data[missing_entries] = true_data[missing_entries]
     return true_data, measured_data
 
-def huntress_reconstruct(file_prefix, fpr, fnr):
-    measured_data_filename = "data/only_fpfn/" + file_prefix + ".SC"
+def huntress_reconstruct(file_prefix, fpr, fnr, only_fpfn = False):
+    if only_fpfn:
+        measured_data_filename = "data/only_fpfn/" + file_prefix + ".SC"
+    else:
+        measured_data_filename = "data/" + file_prefix + ".SC"
     output_name = "temp/" + file_prefix + ".SC"
     fn_fpratio = 51
     fp_coeff = fpr
@@ -55,43 +64,76 @@ def compute_metrics(true_data, reconstruction):
     num_differences = metrics.num_diffs(true_data, reconstruction)
     return np.array([is_ptree, ad_score, df_score, num_differences/true_data.size])
 
+# file_prefixes = [
+# ]
 
-file_prefixes = [
-    ("simNo_10-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.05-na_0.05-d_0-l_1000000", 0.001, 0.05),
-    # ("simNo_5-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.05-na_0.05-d_0-l_1000000",  0.001, 0.05),
-    ("simNo_10-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000",  0.001, 0.2),
-    ("simNo_1-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.05-na_0.05-d_0-l_1000000",  0.001, 0.05),
-    # ("simNo_6-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.05-na_0.05-d_0-l_1000000",  0.001, 0.05),
-    ("simNo_1-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000",   0.001, 0.2),
-    # ("simNo_6-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000",   0.001, 0.2),
-    # ("simNo_7-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.05-na_0.05-d_0-l_1000000",  0.001, 0.05),
-    ("simNo_2-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000",   0.001, 0.2),
-    # ("simNo_7-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000",   0.001, 0.2),
-    # ("simNo_3-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.05-na_0.05-d_0-l_1000000",  0.001, 0.05),
-    # ("simNo_8-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.05-na_0.05-d_0-l_1000000",  0.001, 0.05),
-    # ("simNo_3-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000",   0.001, 0.2),
-    # ("simNo_8-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000",   0.001, 0.2),
-    # ("simNo_4-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.05-na_0.05-d_0-l_1000000",  0.001, 0.05),
-    # ("simNo_9-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.05-na_0.05-d_0-l_1000000",  0.001, 0.05),
-    # ("simNo_4-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000",   0.001, 0.2), 
-    # ("simNo_9-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000",   0.001, 0.2),
-]
+# file_prefixes = file_prefixes.file_prefixes_300x300s
+# file_prefixes = file_prefixes.file_prefixes_1000x300s
+file_prefixes = file_prefixes.file_prefixes_300x1000s
+# file_prefixes = file_prefixes.file_prefixes_1000x1000s
+
+
+curr_time = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M')
+data_file_name = f"data/metrics/metrics_countbased_all_300x1000s_{curr_time}.csv"
+metrics_df = pd.DataFrame(columns=['file', 'n', 'm', 'fpr', 'fnr', 'nar', 'time', 'is_ptree', 'ad_score', 'dl_score', 'fraction_diffs'])
+
+metrics_mat = np.zeros((len(file_prefixes), 4))
+time_start = time.perf_counter()
 
 np.set_printoptions(precision=3)
-for (file_prefix, fpr, fnr) in file_prefixes:
+for i, (file_prefix, n, m, fpr, fnr, nar) in enumerate(file_prefixes):
     print(f"Running file: {file_prefix}")
     true_data, measured_data = load_file(file_prefix)
 
-    print(f"Reconstructing using this method...")
-    this_reconstruction = main.reconstruct(measured_data, fpr, fnr)
+    # print(f"Reconstructing using this method...")
+    # this_reconstruction = main.reconstruct(measured_data, fpr, fnr)
+    # # print(f"Computing this method's metrics...")
+    # m = compute_metrics(true_data, this_reconstruction)
+    # print(m)
+    # metrics_mat[i, :] = m
 
-    print(f"Reconstructing using huntress...")
-    huntress_reconstruction = huntress_reconstruct(file_prefix, fpr, fnr)
+    # print(f"Reconstructing using huntress...")
+    # huntress_reconstruction = huntress_reconstruct(file_prefix, fpr, fnr)
+    # print(f"Computing huntress's metrics...")
+    # hm = compute_metrics(true_data, huntress_reconstruction)
+    # print(hm)
 
-    print(f"Computing this method's metrics...")
-    m = compute_metrics(true_data, this_reconstruction)
-    print(m)
+    # print(f"Reconstructing using count based...")
 
-    print(f"Computing huntress's metrics...")
-    hm = compute_metrics(true_data, huntress_reconstruction)
-    print(hm)
+    t0 = time.perf_counter()
+    separator_reconstruction = CountBasedReconstructor.main_Rec(measured_data, fpr, fnr, nar)
+    separator_reconstruction = CountBasedReconstructor.mlrefinementcol(separator_reconstruction, measured_data, fpr, fnr)
+    t1 = time.perf_counter()
+    sm_after_ml = compute_metrics(true_data, separator_reconstruction)
+    metrics_mat[i, :] = sm_after_ml
+    
+    new_df = pd.DataFrame([[file_prefix, n, m, fpr, fnr, nar, t1 - t0, sm_after_ml[0], sm_after_ml[1], sm_after_ml[2], sm_after_ml[3]]], columns=metrics_df.columns)
+    metrics_df = pd.concat((metrics_df, new_df), ignore_index = True)
+
+    print(sm_after_ml)
+
+time_end = time.perf_counter()
+print(f"Took {time_end - time_start} seconds total")
+
+# print(f"{data_file_name}:")
+# metrics_df.to_csv(data_file_name)
+# np.save(data_file_name, metrics_mat)
+
+np.set_printoptions(precision=3)
+print(f'* FNR: 0.2')
+df_fnr0_2 = metrics_df[np.isclose(metrics_df.fnr, 0.2)][["is_ptree", "ad_score", "dl_score", "fraction_diffs"]]
+times_fnr0_2 = metrics_df[np.isclose(metrics_df.fnr, 0.2)][["time"]]
+print(f'    * Average: {list(df_fnr0_2.mean())}')
+print(f'    * Std Dev: {list(df_fnr0_2.std())}')
+print(f'    * Min:     {list(df_fnr0_2.min())}')
+print(f'    * Max:     {list(df_fnr0_2.max())}')
+print(f'    * Time: {list(times_fnr0_2.mean())[0]} average, {list(times_fnr0_2.std())[0]} std')
+
+print(f'* FNR: 0.05')
+df_fnr0_05 = metrics_df[np.isclose(metrics_df.fnr, 0.05)][["is_ptree", "ad_score", "dl_score", "fraction_diffs"]]
+times_fnr0_05 = metrics_df[np.isclose(metrics_df.fnr, 0.05)][["time"]]
+print(f'    * Average: {list(df_fnr0_05.mean())}')
+print(f'    * Std Dev: {list(df_fnr0_05.std())}')
+print(f'    * Min:     {list(df_fnr0_05.min())}')
+print(f'    * Max:     {list(df_fnr0_05.max())}')
+print(f'    * Time: {list(times_fnr0_05.mean())[0]} average, {list(times_fnr0_05.std())[0]} std')
--- src/d02_processing/main.py
+++ src/d02_processing/main.py
@@ -1,3 +1,4 @@
+from tracemalloc import start
 import numpy as np
 import scipy.stats
 
@@ -18,31 +19,38 @@ def read_data(filename):     # reads a matrix from file and returns it in BOOL t
 def prob_equal(p_i, p): 
     assert p_i.shape[1] == 1 and p_i.shape[0] == p.shape[0]
     return np.prod(p_i*p + (1-p_i)*(1-p), axis=0)
+    # return np.exp(np.sum(np.log(p_i*p + (1-p_i)*(1-p)), axis=0))
 
 def prob_superset(p_i, p): # supseteq
     assert p_i.shape[1] == 1 and p_i.shape[0] == p.shape[0]
     return np.prod(1-(1-p_i)*p, axis=0)
+    # return np.exp(np.sum(np.log(1-(1-p_i)*p), axis=0))
 
 def prob_subset(p_i, p): # subseteq
     assert p_i.shape[1] == 1 and p_i.shape[0] == p.shape[0]
     return np.prod(1-(1-p)*p_i, axis=0)
+    # return np.exp(np.sum(np.log(1-(1-p)*p_i), axis=0))
 
 def prob_disjoint(p_i, p):
     assert p_i.shape[1] == 1 and p_i.shape[0] == p.shape[0]
     return np.prod(1-p_i*p, axis=0)
+    # return np.exp(np.sum(np.log(1-p_i*p), axis=0))
 
-def pick_pivot(prob_mat):
+def pick_pivot(prob_mat, available_cols):
     # Return (pivot, index in prob_mat)
     
-    # This picks column that maximizes the expected number of 1s
+    # This picks column that maximizes the expected number of 1s of the available columns
+    # Probably a smarter way to ensure that when matrix is all 0s, available columns are not picked
+    prob_mat = prob_mat * available_cols  - np.ones_like(prob_mat)*(1-available_cols)
     expected_num_ones = np.sum(prob_mat, axis=0)
 
     # Of the ones that have the same number of expected ones, pick one that is a superset of others.
     max_val = np.max(expected_num_ones)
     start_idx = np.argmax(expected_num_ones) # Get the first index with the most expected ones.
+    assert available_cols[start_idx] == 1
     pivot_candidate_idx = start_idx
     for i in range(start_idx + 1, prob_mat.shape[1]):
-        if expected_num_ones[i] != max_val:
+        if expected_num_ones[i] != max_val or not available_cols[i]:
             pass
         pivot = prob_mat[:, pivot_candidate_idx].reshape((prob_mat.shape[0], 1))
         new = prob_mat[:, i].reshape((prob_mat.shape[0], 1))
@@ -122,7 +130,7 @@ def pick_pivot(prob_mat):
 #     # print('true col', true_col)
 #     return true_col.astype(int)
 
-def find_subtree(pivot, prob_mat):
+def find_subtree(pivot, prob_mat, available_cols):
     subtree = np.zeros(prob_mat.shape[1])
     changed_pivot = True
     while changed_pivot:
@@ -143,7 +151,7 @@ def find_subtree(pivot, prob_mat):
 
         entropy = scipy.stats.entropy(joined, base=3, axis=0)
         for (i, _) in sorted(enumerate(entropy), key=lambda x: x[1]):
-            if subtree[i] == 1:
+            if subtree[i] == 1 or not available_cols[i]:
                 continue
             prob_max = np.argmax(joined[:, i])
             if prob_max == 0: # sup
@@ -157,12 +165,13 @@ def find_subtree(pivot, prob_mat):
                 pass
     return subtree
 
-def reconstruct_root(pivot, subtree, prob_mat):
+def reconstruct_root(pivot, subtree, prob_mat, available_cols):
     pr1 = np.log(1 - pivot).reshape((pivot.shape[0],))
     pr1 += np.sum(np.log(1 - prob_mat*subtree), axis=1)
     pr1 = 1 - np.exp(pr1)
 
-    disjoints = 1 - subtree
+    disjoints = (1 - subtree)*available_cols
+    assert np.all(disjoints.shape == subtree.shape)
     pr0 = 1 - np.exp(np.sum(np.log(1 - prob_mat*disjoints), axis=1))
     
     # print(' ')
@@ -175,15 +184,15 @@ def reconstruct_root(pivot, subtree, prob_mat):
     # what if do some ands of different conditions
     return root
 
-def backward_pass(prob_mat, true_col, subtree):
+def backward_pass(prob_mat, true_col, subtree, available_cols):
     assert true_col.shape == (prob_mat.shape[0], 1)
     assert subtree.shape == (prob_mat.shape[1],)
 
-    prob_mat = prob_mat*(1-subtree)*(1-true_col) + prob_mat*subtree*true_col
+    prob_mat = prob_mat*(1-subtree)*available_cols*(1-true_col) + prob_mat*subtree*true_col
     return prob_mat
 
-def prob_measure(x, Y, fpr, fnr):
-    # Probability of measuring vector x given true vector y_i with Y = [y_1 ...]
+def prob_measure(x, Y, fpr, fnr): 
+    # Log of probability of measuring vector x given true vector y_i with Y = [y_1 ...]
     assert x.shape[1] == 1
     p00 = 1-fpr
     p01 = fpr
@@ -191,9 +200,25 @@ def prob_measure(x, Y, fpr, fnr):
     p11 = 1-fnr
     element_probs = (1-x)*(1-Y)*p00 + x*(1-Y)*p01 + (1-x)*Y*p10 + x*Y*p11
     assert element_probs.shape == Y.shape
-    probs = np.prod(element_probs, axis=0)
+    # probs = np.prod(element_probs, axis=0)
+    probs = np.sum(np.log(element_probs), axis=0)
     assert probs.shape == (Y.shape[1],)
     return probs
+
+def mlrefinementcol(M,M_noisy,p01,p10):  # this is for non missing element case M is reconstructed matrix, M_noisy is the noisy input
+    M_output=M_noisy.copy()
+    # print(M,M_noisy)
+    for i in range(M_output.shape[1]):
+        p=[]
+        for j in range(M_output.shape[1]):
+            #print(i,j,np.sum(M_output[:,i]>M[:,j])*np.log(p01)+np.sum(M_output[:,i]<M[:,j])*np.log(p10))
+            p.append(np.sum(M_output[:,i]>M[:,j])*np.log(p01)+np.sum(M_output[:,i]<M[:,j])*np.log(p10))
+    #print(M_output[:,i],M[:,np.argmax(p)], " are matched" )
+        # print(i,np.argmax(p), " are matched" )
+    
+        M_output[:,i]=M[:,np.argmax(p)]
+    
+    return M_output
 
 def reconstruct(measured, fpr, fnr):
     # measured_frac1 = np.sum(measured, axis=0) / measured.shape[0]
@@ -213,7 +238,7 @@ def reconstruct(measured, fpr, fnr):
     # assert not np.any(np.isnan(prob_mat))
     # prob_mat = np.clip(prob_mat, 0, 1)
 
-    # Estimate prior over whole matrix
+    # Estimate prior over whole matrix (TODO: double check equiv to Can's code)
     measured_num0 = np.sum(1-measured)
     measured_num1 = np.sum(measured)
     est_true_num1 = 1/(fpr*fnr - (1-fpr)*(1-fnr)) * (fpr*measured_num0 - (1-fpr)*measured_num1)
@@ -221,7 +246,6 @@ def reconstruct(measured, fpr, fnr):
     prob_mat = measured * ((1-fnr)*est_true_num1/measured_num1) + (1-measured)*(1-(1-fpr)*est_true_num0/measured_num0)
     assert not np.any(np.isnan(prob_mat))
     prob_mat = np.clip(prob_mat, 0, 1)
-    
     
     # print(prob_mat)
     assert np.all(prob_mat >= 0)
@@ -233,38 +257,60 @@ def reconstruct(measured, fpr, fnr):
 
     completed_cols = np.empty_like(measured)
     completed_cols_i = 0
+    available_cols = np.ones(prob_mat.shape[1])
 
-    while prob_mat.shape[1] > 1: # While more than 1 column remaining.
-        pivot, pivot_idx = pick_pivot(prob_mat)
-        prob_mat = np.delete(prob_mat, pivot_idx, 1)
+    # while prob_mat.shape[1] > 1: # While more than 1 column remaining.
+    while np.sum(available_cols) > 1:
+        pivot, pivot_idx = pick_pivot(prob_mat, available_cols)
+        # prob_mat = np.delete(prob_mat, pivot_idx, 1) # figure out which column to remove / replace based on the reconstructed pivot
+        assert available_cols[pivot_idx]
+        # available_cols[pivot_idx] = 0
 
         # print(f'pivot: {pivot_idx}, \n{pivot}')
-        subtree = find_subtree(pivot, prob_mat)
+        subtree = find_subtree(pivot, prob_mat, available_cols)
 
         # print(f'subtree after unions: \n{subtree}')
-        reconstructed_root = reconstruct_root(pivot, subtree, prob_mat)
+        reconstructed_root = reconstruct_root(pivot, subtree, prob_mat, available_cols)
         # print(f'01d pivot: \n{reconstructed_root}')
-        prob_mat = backward_pass(prob_mat, reconstructed_root, subtree)
+
+        p00 = 1-fpr
+        p01 = fpr
+        p10 = fnr
+        p11 = 1-fnr
+        element_probs = (1-reconstructed_root)*(1-measured)*p00 + (1-reconstructed_root)*measured*p01 + reconstructed_root*(1-measured)*p10 + reconstructed_root*measured*p11
+        probs = np.exp(np.sum(np.log(element_probs), axis=0))
+        probs = probs*available_cols
+        max_idx = np.argmax(probs)
+        assert available_cols[max_idx]
+        # available_cols[max_idx] = 0
+
+        available_cols[pivot_idx] = 0
+
+        prob_mat = backward_pass(prob_mat, reconstructed_root, subtree, available_cols)
 
         completed_cols[:, completed_cols_i] = reconstructed_root.reshape((reconstructed_root.shape[0],))
         completed_cols_i += 1
 
-    completed_cols[:, -1] = np.around(prob_mat).reshape((prob_mat.shape[0],))
+    remaining_col_idx = np.argmax(available_cols)
+    completed_cols[:, -1] = np.around(prob_mat[:, remaining_col_idx]).reshape((prob_mat.shape[0],))
 
     reconstructed = np.empty_like(measured)
     for i in range(measured.shape[1]):
         prob_measured = prob_measure(measured[:, i].reshape((measured.shape[0], 1)), completed_cols, fpr, fnr)
         max_idx = np.argmax(prob_measured)
         reconstructed[:, i] = completed_cols[:, max_idx]
-        completed_cols = np.delete(completed_cols, max_idx, 1)
-
+        # completed_cols = np.delete(completed_cols, max_idx, 1)
     return reconstructed
+
+    # refined = mlrefinementcol(completed_cols, measured, fpr, fnr)
+    # return refined
 
 
 def main():
 
     # Test with actual data
-    file_name = "simNo_10-s_100-m_1000-h_1-minVAF_0.005-ISAV_0-n_1000-fp_0.001-fn_0.05-na_0.05-d_0-l_1000000"
+    file_name = "simNo_10-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000"
+    # file_name = "simNo_10-s_100-m_1000-h_1-minVAF_0.005-ISAV_0-n_1000-fp_0.001-fn_0.05-na_0.05-d_0-l_1000000"
     # file_name  = "simNo_1-s_100-m_300-h_1-minVAF_0.005-ISAV_0-n_300-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000"
     # file_name = "simNo_10-s_100-m_1000-h_1-minVAF_0.005-ISAV_0-n_1000-fp_0.001-fn_0.2-na_0.05-d_0-l_1000000"
     true_data_filename = "data/" + file_name + ".SC.before_FP_FN_NA"
@@ -281,9 +327,12 @@ def main():
     reconstructed = reconstruct(measured_data, fpr, fnr)
 
     print(f"Number of elements in matrix: {true_data.shape[0] * true_data.shape[1]}")
-    metrics.isPtree(reconstructed)
-    metrics.compareAD(true_data, reconstructed)
-    metrics.compareDF(true_data, reconstructed)
+    is_ptree = metrics.isPtree(reconstructed)
+    print(f"Is PTree: {is_ptree}")
+    (_, _, _, ad_score) = metrics.compareAD(true_data, reconstructed)
+    print(f"AD Score: {ad_score}")
+    (_, _, df_score) = metrics.compareDF(true_data, reconstructed)
+    print(f"DF Score: {df_score}")
     print(f"Number of differences: {metrics.num_diffs(true_data, reconstructed)}")
 
     # # for basic testing
